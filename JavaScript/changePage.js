b911a5e5866fa13f9080cea8357a1cba33c3801e
zFishStick <gabri.pesce0@gmail.com>
2024-09-07 14:46:10 +0200

    modifichine

--- JavaScript/changePage.js
+++ JavaScript/changePage.js
@@ -1,3 +1,8 @@
-function changePage() {
-    window.location.href = "Pages/library.html";
+function changePage(clicked_id) {
+    if (clicked_id == "go-to-library"){
+        window.location.href = "Pages/library.html";
+    }
+    else if(clicked_id == "back-to-index"){
+        window.location.href = "../index.html";
+    }
 }
